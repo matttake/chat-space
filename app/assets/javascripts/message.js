f2f3df6b16f1ecd7d00f3a25ea4bc49fd4e17e8c
Yuto Tsukamoto <t.fr.swim@icloud.com>
2020-05-05 17:02:20 +0900

    自動更新、修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,28 +65,32 @@ $(function(){
   })
 
   var reloadMessages = function() {
-  var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-     alert('error');
-    });
- };
- if (document.location.href.match(/\/groups\/\d+\/messages/)) {
- setInterval(reloadMessages, 7000);
- }
+    var last_message_id = $('.Main__Chats__Message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.Main__Chats').append(insertHTML);
+          $('.Main__Chats').animate({ scrollTop: $('.Main__Chats')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+      alert('error');
+      });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
+
+
+
+
